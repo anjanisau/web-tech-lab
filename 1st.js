65ec51ea48cb1ed86000af77bd3395c39ba537c5
anjanisau <55202171+anjanisau@users.noreply.github.com>
2021-04-05 02:01:47 +0530

    Add files via upload

---
+++ 1st.js
@@ -0,0 +1,26 @@
+console.log("hello world");//printing  on inspect console
+//alert("submitted") //alter
+//console.warn("this is giving warning");
+//console.error("for error")
+document.write("  printing on browser")
+
+//data types
+var num1=10;
+var num2=11;
+console.log(num1+num2);
+var str1="anjani";
+var str2='saurav';
+//console.log(str1+str2);
+//object data type
+var mark = {
+    ravi : 34,
+    shubam: 55,
+    saurav: 96
+}
+var und
+console.log(undefined)//defult it take undefined
+//high level there are two types of 1
+//1)primative
+//2)reference
+var arr=[1,2,3,4]
+console.log(arr[0])
